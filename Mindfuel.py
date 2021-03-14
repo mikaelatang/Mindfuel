ed4e8c4c13024004407a7ee329fc8ffb173fea07
mikaelatang <57931271+mikaelatang@users.noreply.github.com>
2021-03-14 09:17:58 -0400

    Update Mindfuel.py

--- Mindfuel.py
+++ Mindfuel.py
@@ -1,12 +1,10 @@
-import folium
-import os
 import json
 import requests
 import pandas as pd
 import folium.plugins as plugins
 import csv
 
-# CO2 emission in kg/gallon constant
+# CO2 emission in lb/gallon constant
 emission = 19.6
 
 # model and make (inputted when signing in)
@@ -19,11 +17,11 @@ with open('database.csv', 'r') as csv_file:
     for row in csv_reader:
         if (row["Make"] == make) & (row["Model"] == model):
             # create map
-            map = folium.Map(location=[49.242371, -123.134354], zoom_start=14.1, tiles='Stamen Terrain')
+            map = folium.Map(location=[43.757249, -79.422801], zoom_start=16, tiles='Stamen Terrain')
 
             tooltip='Click for more info'
 
-            miles_travelled = 3.51
+            miles_travelled = 1.06
             
             carbon_emission = round((emission * miles_travelled / int(row["Combined MPG (FT1)"])) * 0.453592, 2)
             
@@ -31,12 +29,12 @@ with open('database.csv', 'r') as csv_file:
             overlay = os.path.join('data', 'map.geojson')
 
             # add markers
-            folium.Marker([49.246686, -123.177293],
+            folium.Marker([43.756859, -79.430802],
                           popup='Estimated Carbon Emission: ' + str(carbon_emission) + 'kg',
                           tooltip='Destination',
                           icon=folium.Icon(color='green', icon='leaf')).add_to(map)
-            folium.Marker([49.243060, -123.115028],
-                          tooltip='This is you',
+            folium.Marker([43.757416, -79.413744],
+                          tooltip='You are here',
                           icon=folium.Icon(color='green', icon='leaf')).add_to(map)
                           
             folium.GeoJson(overlay, name='Shortest Route').add_to(map)
